675d80d3ad25f59d4eafdf5952b134a0bc26781d
Cozmu <www.jorgereis@gmail.com>
2023-02-16 17:07:00 -0300

    fix: error de registro

--- tests/unit/controllers/salesController.test.js
+++ tests/unit/controllers/salesController.test.js
@@ -6,13 +6,14 @@ const { expect } = chai;
 chai.use(sinonChai);
 
 const { salesController } = require('../../../src/controllers');
-const { salesService } = require('../../../src/services');
+const salesService = require('../../../src/services/sales.service');
 const {
   invalidInput,
   invalidQuantity,
   newSale,
   productNotFound,
-  requestNewSale
+  requestNewSale,
+  allSales
 } = require('./mocks/sales.controller.mocks');
 
 describe('CONTROLLER - Desenvolva testes que cubram no mínimo 15% das camadas da sua aplicação', function () {
@@ -66,6 +67,7 @@ describe('CONTROLLER - Desenvolva testes que cubram no mínimo 15% das camadas d
     });
 
     it('Verifique se é possível cadastrar uma venda com sucesso', async function () {
+      sinon.restore();
       const req = { body: requestNewSale };
       const res = {};
 
@@ -74,17 +76,69 @@ describe('CONTROLLER - Desenvolva testes que cubram no mínimo 15% das camadas d
       sinon
         .stub(salesService, 'registerSale')
         .resolves({ type: null, message: newSale });
-
+        
+      // console.log(newSale);
       await salesController.registerSale(req, res);
+
 
       expect(res.status).to.have.been.calledWith(201);
       expect(res.json).to.have.been.calledWith(newSale);
     });
 
-    
-    
     afterEach(function () {
+      // salesService.registerSale.restore();
       sinon.restore();
     });
   });
 });
+
+describe('CONTROLLER - Desenvolva testes que cubram no mínimo 20% das camadas da sua aplicação', function () {
+  describe('CONTROLLER - Validando se a cobertura total das linhas e funções dos arquivos de CADA camada models, services e controllers é maior ou igual a 20%', function () {
+    it('Verifica se é possível listar todas as vendas na tora /sales com metodo "GET"', async function () {
+      const req = {};
+      const res = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon
+        .stub(salesService, 'getAllSalesProducts')
+        .resolves({ type: null, message: allSales });
+      
+      await salesController.listSales(req, res);
+
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith([
+        {
+          "saleId": 1,
+          "productId": 1,
+          "date": "2023-02-16T18:32:28.000Z",
+          "quantity": 5
+        },
+        {
+          "saleId": 1,
+          "productId": 2,
+          "date": "2023-02-16T18:32:28.000Z",
+          "quantity": 10
+        },
+        {
+          "saleId": 2,
+          "productId": 3,
+          "date": "2023-02-16T18:32:28.000Z",
+          "quantity": 15
+        },
+        {
+          "saleId": 3,
+          "productId": 1,
+          "date": "2023-02-16T18:34:50.000Z",
+          "quantity": 1
+        },
+        {
+          "saleId": 3,
+          "productId": 2,
+          "date": "2023-02-16T18:34:50.000Z",
+          "quantity": 5
+        }
+      ]);
+    });
+  });
+}); 
